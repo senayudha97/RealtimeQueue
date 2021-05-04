387c6eafe24b90b29db1c791de221a8f18092a55
Sena Yudha <senayudha97@outlook.com>
2021-05-04 15:52:05 +0700

    front end  notifikasi antrian warga

---
+++ admin/application/template_email/template_antrian.php
@@ -0,0 +1,57 @@
+<html>
+<!-- #A3D0F8 -->
+
+<body style="color: #000; font-size: 16px; text-decoration: none; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; background-color: #efefef;">
+
+    <div id="wrapper" style="max-width: 600px; margin: auto auto; padding: 20px;">
+
+        <div id="logo" style="">
+            <center>
+                <h1 style="margin: 0px;"><a href="#"><img style="max-height: 75px;" src=""></a></h1>
+            </center>
+        </div>
+
+        <div id="content" style="font-size: 16px; padding: 25px; background-color: #fff;
+				moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; -khtml-border-radius: 10px;
+				border-color: #ce3c30; border-width: 4px 1px; border-style: solid;">
+
+            <h1 style="font-size: 22px;">
+                <center>{JUDUL}</center>
+            </h1>
+
+            <h3 style="text-align: center;">Berikut Nomor Antrian Anda</h3>
+            <table style="margin: auto;" border="1" cellspacing="0" cellpadding="10px">
+                <tr>
+                    <td>
+                        <center>
+                            <p style="font-weight: bold; font-size: 75px;">{NOMOR_URUT}</p>
+                            <div style="font-size: 13px;">
+                                <p>NAMA : {NAMA}</p>
+                                <p>KEPENGURUSAN : {KEPENGURUSAN}</p>
+                            </div>
+                            <p>SIMPAN EMAIL INI DAN TUNJUKAN PADA PETUGAS <br> SAAT ANTRIAN ANDA DI PANGGIL</p>
+                        </center>
+                    </td>
+                </tr>
+            </table>
+            <!-- <p>{MESSAGE}</p> -->
+
+
+
+            <p style="display: flex; justify-content: center; margin-top: 10px;">
+                <center>
+                    <!-- <a href="http://priv.itn.ac.id/masteritn" target="_blank" style="border: 1px solid #0561B3; background-color: #238CEA; 
+					color: #fff; text-decoration: none; font-size: 18px; padding: 10px 20px;">Lakukan cek ke Masteritn</a> -->
+        </div>
+        </center>
+        </p>
+
+    </div>
+
+    <!-- <div id="footer" style="margin-bottom: 20px; padding: 0px 8px; text-align: center;">
+        even add footer links to your <a href="https://www.heytuts.com/web-dev" target="_blank" style="text-decoration: none; color: #238CEA;">website</a> from these emails
+    </div> -->
+    </div>
+</body>
+
+</html>
