6581043ea6b0fa9589025eb8e961068e4e3c7f0f
Sena Yudha <senayudha97@outlook.com>
2021-05-04 16:21:38 +0700

    perbaikan tampilan siantrian utama

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
           <input type="text" name="input[jenis]" value="1">
         </div>
         <div class="modal-body">
-          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class="rounded-circle bg-danger text-light" id="ktpready1"></button></h5>
+          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class=" btn btn-sm font-weight-bold bg-danger text-light" id="ktpready1"></button></h5>
           <input type="text" class="form-control resetable" id="name1" name="input[nama]" placeholder="Nama" required data-error="Isikan Nama anda">
           <input type="tel" class="form-control resetable" id="phone1" name="input[nohp]" placeholder="No. Handphone" required data-error="Isikan No. Handphone anda">
           <input type="text" class="form-control resetable" id="email1" name="input[email]" placeholder="Email" required data-error="Isikan Alamat Email anda">
@@ -183,7 +183,7 @@
           <input type="text" name="input[jenis]" value="2">
         </div>
         <div class="modal-body">
-          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class="rounded-circle bg-danger text-light" id="ktpready2"></button></h5>
+          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class=" btn btn-sm font-weight-bold bg-danger text-light" id="ktpready2"></button></h5>
           <input type="text" class="form-control resetable" id="name2" name="input[nama]" placeholder="Nama" required data-error="Isikan Nama anda">
           <input type="tel" class="form-control resetable" id="phone2" name="input[nohp]" placeholder="No. Handphone" required data-error="Isikan No. Handphone anda">
           <input type="email" class="form-control resetable" id="email2" name="input[email]" placeholder="Email" required data-error="Isikan Alamat Email anda">
@@ -217,7 +217,7 @@
           <input type="text" name="input[jenis]" value="1">
         </div>
         <div class="modal-body">
-          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class="rounded-circle bg-danger text-light" id="kkready1"></button></h5>
+          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class="btn btn-sm font-weight-bold bg-danger text-light" id="kkready1"></button></h5>
           <input type="text" class="form-control resetable" id="name3" name="input[nama]" placeholder="Nama" required data-error="Isikan Nama anda">
           <input type="tel" class="form-control resetable" id="phone3" name="input[nohp]" placeholder="No. Handphone" required data-error="Isikan No. Handphone anda">
           <input type="email" class="form-control resetable" id="email3" name="input[email]" placeholder="Email" required data-error="Isikan Alamat Email anda">
@@ -270,7 +270,7 @@
           <input type="text" name="input[jenis]" value="2">
         </div>
         <div class="modal-body">
-          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class="rounded-circle bg-danger text-light" id="kkready2"></button></h5>
+          <h5 class="text-dark">Antrian Tersedia : <button onclick="event.preventDefault()" class=" btn btn-sm font-weight-bold bg-danger text-light" id="kkready2"></button></h5>
           <input type="text" class="form-control resetable" id="name4" name="input[nama]" placeholder="Nama" required data-error="Isikan Nama anda">
           <input type="tel" class="form-control resetable" id="phone4" name="input[nohp]" placeholder="No. Handphone" required data-error="Isikan No. Handphone anda">
           <input type="email" class="form-control resetable" id="email4" name="input[email]" placeholder="Email" required data-error="Isikan Alamat Email anda">
