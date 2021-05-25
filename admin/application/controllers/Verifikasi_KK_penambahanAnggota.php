40655a428baf67aeb51109821eb885bc12749f1f
Sena Yudha <senayudha97@outlook.com>
2021-05-25 14:45:07 +0700

    KTP dan rancangan KK

---
+++ admin/application/controllers/Verifikasi_KK_penambahanAnggota.php
@@ -0,0 +1,76 @@
+<?php
+include_once APPPATH . "/controllers/Guide.php";
+
+
+class Verifikasi_KK_penambahanAnggota extends Guide
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Tbl_verifikasi_kk");
+        is_logged_in();
+    }
+
+    public function index()
+    {
+
+        $data['title'] = get_title($this->uri->segment(1));
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->db->order_by('tanggal_antrian', 'ASC');
+        $this->db->where('is_delete', $this->is_delete);
+        $this->db->where('status ', 0);
+        $this->db->where('tanggal_antrian ', date('Y-m-d'));
+        $data['data'] = $this->db->get('antrian_kk')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('proses_antrian/Verifikasi_antrian_kk_view', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function proses($param = "")
+    {
+        $data['title'] = get_title($this->uri->segment(1));
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->db->order_by('tanggal_antrian', 'ASC');
+        $this->db->where('is_delete', $this->is_delete);
+        $this->db->where('id', $param);
+        $data['data'] = $this->db->get('antrian_kk')->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('proses_antrian/Verifikasi_antrian_kk_proses', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function verif($id = "")
+    {
+        $this->db->where('id', $id);
+        $this->db->set('status', 2);
+        if ($this->db->update('antrian_kk')) {
+            $this->db->where('id', $id);
+            $user = $this->db->get('antrian_kk')->row_array();
+            $qrval = $user['nama'] . ',' . $user['email'] . ',' . $user['nohp'] . ',' . 'sa7d4c44a3ajads6ddd445ca0d1b65ca' . ',' . 'kk' . ',' . $id;
+            $this->sendEmail(['email' => $user['email'], 'message' => "Pengajuan antrian anda telah diverifikasi oleh petugas, Simpan QR Code lalu datang ke kantor Dinas Kependudukan dan Pencatatan Sipil Kabupaten Mojokerto untuk scan dan mendapat antrian anda.", 'qrval' => $qrval, 'istolak' => false]);
+            $this->flash_success("Proses Verifikasi KK Berhasil");
+            redirect('verifikasi_antrian_kk');
+        }
+    }
+    public function tolak()
+    {
+        $id = $_POST['id'];
+        $catatan = (!empty($_POST['catatan'])) ? $_POST['catatan'] : "Pengajuan anda di tolak";
+
+        $this->db->where('id', $id);
+        $this->db->set('status', 1);
+        $this->db->set('catatan_penolakan', $catatan);
+        if ($this->db->update('antrian_kk')) {
+            $user = $this->db->get('antrian_ktp')->row_array();
+            $this->sendEmail(['email' => $user['email'], 'message' => $catatan, 'istolak' => true]);
+            $this->flash_success("Proses Tolak KK Berhasil");
+            redirect('verifikasi_antrian_kk');
+        }
+    }
+}
---
+++ admin/application/views/proses_antrian/verifikasi_KK_penguranganAnggota_view.php
@@ -0,0 +1,86 @@
+<div class="container-fluid">
+    <h2 class="mb-5"><?= $title; ?></h2>
+
+    <?php if (validation_errors()) : ?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <?= validation_errors(); ?>
+        </div>
+    <?php endif; ?>
+
+    <?= $this->session->flashdata('message'); ?>
+    <form action="<?= base_url('antrian_bulanan/report'); ?>" method="POST" target="_blank">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col-sm-10 mb-3">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!-- <input type="text" class="form-control easydatepicker" name="tgl[awal]" value="<?= date('d-m-Y'); ?>"> -->
+                                <input type="text" class="form-control easydatepicker" id="valtable" value="<?= date('d-m-Y'); ?>">
+                            </div>
+                            <div class="col-sm-6">
+                                <button class="btn  btn-danger" id="search"><i class="fas fa-search"></i></button>
+                                <!-- <input type="text" class="form-control easydatepicker" name="tgl[akhir]" value="<?= date('d-m-Y'); ?>"> -->
+                                <!-- <button type="submit" class="btn btn-danger "><span class="fas fa-fw fa-file-pdf"></span>PDF</button> -->
+                                &nbsp;
+                                <!-- <button type="submit" name="excel" class="btn btn-success "><span class="fas fa-fw fa-file-excel"></span>Excel</button> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <table id="table_id" class="table table-bordered nowrap" cellspacing="0" width="100%">
+                <thead>
+                    <tr>
+                        <th class="tg-0lax">No</th>
+                        <th class="tg-0lax">Tanggal Antri</th>
+                        <th class="tg-0lax">Nama</th>
+                        <th class="tg-0lax">Nomor HP</th>
+                        <th class="tg-0lax">Email</th>
+                        <th class="tg-0lax">Jenis Antrian</th>
+                        <th class="tg-0lax" width="10%"><span class="fas fa-fw fa-edit"></span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($data as $value) : ?>
+                        <tr>
+                            <td scope="row"><?= $i; ?></td>
+                            <td><?= content_date($value['tanggal_antrian']); ?></td>
+                            <td><?= $value['nama']; ?></td>
+                            <td><?= $value['nohp']; ?></td>
+                            <td><?= $value['email']; ?></td>
+                            <td><?= $value['jenis'] == 1 ?  'Baru' : 'Perubahan'; ?></td>
+                            <td>
+                                <a href="<?= base_url("verifikasi_antrian_ktp/proses/" . $value['id']); ?>" class="btn btn-success rounded-left"><span class="fas fa-fw fa-check"></span>Proses Antrian</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        var table = $('#table_id').DataTable({
+            "scrollX": true,
+        });
+
+        $('#search').click(function() {
+            event.preventDefault();
+            table.search($('#valtable').val()).draw();
+        });
+    });
+</script>
