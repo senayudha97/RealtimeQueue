aec83b9081fdb82c9064007eb3dd80bf70e357f2
Sena Yudha <senayudha97@outlook.com>
2021-07-18 22:50:29 +0700

    add floating button for CS

--- header.php
+++ header.php
@@ -150,3 +150,25 @@
       </div>
     </div>
   </header>
+
+  <style>
+    .float {
+      position: fixed;
+      width: 60px;
+      height: 60px;
+      bottom: 60px;
+      right: 40px;
+      background-color: #0C9;
+      color: #FFF;
+      border-radius: 50px;
+      text-align: center;
+      box-shadow: 2px 2px 3px #999;
+    }
+
+    .my-float {
+      margin-top: 22px;
+    }
+  </style>
+  <a href="#" class="float">
+    <i class="fas fa-phone my-float"></i>
+  </a>
